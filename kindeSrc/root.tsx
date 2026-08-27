541872489fcbd46b84eaaf09a6c99fc6204de60f
Alexis Pesicka <14352901+pesickaa@users.noreply.github.com>
2026-08-27 13:16:02 +0100

    chore: attempt to use pageTitle over pageTitle like docs suggest

--- kindeSrc/root.tsx
+++ kindeSrc/root.tsx
@@ -27,7 +27,7 @@ export const Root = ({
         <meta content={getKindeCSRF()} name="csrf-token" />
         <meta content="light" name="color-scheme" />
         <meta content="nopagereadaloud" name="google" />
-        <title data-page-title>{context.widget.content.page_title}</title>
+        <title data-page-title>{context.widget.content.pageTitle}</title>
 
         <link href={getSVGFaviconUrl()} rel="icon" type="image/svg+xml" />
 
